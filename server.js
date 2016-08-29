98ba22bb9664c38da60596b88cbbc3a4097b8dfb
Joseph Abrahams <joseph@abrahams.io>
2016-08-29 11:39:37 -0400

    add cors

--- server.js
+++ server.js
@@ -9,6 +9,12 @@ var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 server.listen(process.env.PORT || 5000);
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
